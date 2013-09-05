384a46df59be56c42853b437708e48a6836d8168
liyuanhong <liyh@witmob.com>
2013-09-05 20:45:11 +0800

    增加了发送消息的线程

---
+++ src/com/yuanhong/sendMessage/SendMessageThread.java
@@ -0,0 +1,40 @@
+package com.yuanhong.sendMessage;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class SendMessageThread extends Thread{
+	private String message;
+	private Socket socket;
+	private String address;
+	private int port;
+	
+	SendMessageThread(String message,String address,int port) {
+		this.message=message;
+		this.address=address;
+		this.port=port;
+		
+		try {
+			socket=new Socket(address, port);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void run() {
+		try {			
+			OutputStreamWriter output = new OutputStreamWriter(socket.getOutputStream());
+			output.write(message);
+			output.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
