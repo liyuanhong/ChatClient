e6f15006564bc0a5429b6fa6147879b901370293
liyuanhong <liyh@witmob.com>
2013-09-17 13:06:56 +0800

    处理了用户名重复的bug

--- src/com/yuanhong/listener/LoginViewConfigerClick.java
+++ src/com/yuanhong/listener/LoginViewConfigerClick.java
@@ -3,10 +3,12 @@ package com.yuanhong.listener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Locale;
 
+import javax.net.ssl.SSLEngineResult.Status;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
@@ -15,6 +17,7 @@ import org.json.JSONObject;
 
 import com.yuanhong.service.SendMessageThread;
 import com.yuanhong.ui.ChatClient;
+import com.yuanhong.util.AnalyseMessage;
 import com.yuanhong.util.MessageClass;
 import com.yuanhong.util.MessageType;
 
@@ -39,27 +42,29 @@ public class LoginViewConfigerClick extends MouseAdapter{
 	}
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		super.mouseClicked(e);
-		ChatClient client = new ChatClient(loginName.getText(),serverAddress.getText(),Integer.parseInt(theServerPort.getText()));
-		client.showWin();
+		super.mouseClicked(e);		
 		
 		message = new MessageClass();		
 		message.setUserName(loginName.getText());
 		message.setMessType(MessageType.LOGIN);
-		System.out.println(clientPortField.getText());
 		if(clientPortField.isEditable()){
 			message.setClientPort(getUserDefinedPort());	
-			System.out.println(clientPortField.isEditable() + "-----------");
 		}else{
 			message.setClientPort(getDefaultPort());
-			System.out.println(clientPortField.isEditable() + "+++++++++++++");
+			clientPort = getDefaultPort();
 		}
 		
 		JSONObject json = new JSONObject(message.getJsonMap());	
 		SendMessageThread thread = new SendMessageThread(json.toString(), serverAddress.getText(), Integer.parseInt(theServerPort.getText()));
 		thread.start();
-		
-		window.dispose();
+		int temp = getUserStatus();
+		if(temp == 1){
+			JOptionPane.showMessageDialog(window, "该用户名已经使用，请换其他用户名！", "警告", JOptionPane.ERROR_MESSAGE);
+		}else if(temp == 0){
+			ChatClient client = new ChatClient(loginName.getText(),serverAddress.getText(),Integer.parseInt(theServerPort.getText()));
+			client.showWin();
+			window.dispose();
+		}		
 	}
 	
 	//判断将要作为获取消息的端口号是否被占用
@@ -90,14 +95,35 @@ public class LoginViewConfigerClick extends MouseAdapter{
 		try{
 			clientPort = Integer.parseInt(clientPortField.getText());
 			if(1024 > clientPort && clientPort > 49151){
-				JOptionPane.showConfirmDialog(window, "请输入1024~49151", "警告", JOptionPane.ERROR_MESSAGE);
+				JOptionPane.showMessageDialog(window, "请输入1024~49151", "警告", JOptionPane.ERROR_MESSAGE);
 				return 0;    //返回0，表示获取端口号错误，重新输入
 			}else{
 				return clientPort;
 			}
 		}catch(Exception e){
-			JOptionPane.showConfirmDialog(window, "请输入1024~49151的数字", "警告", JOptionPane.ERROR_MESSAGE);
+			JOptionPane.showMessageDialog(window, "请输入1024~49151的数字", "警告", JOptionPane.ERROR_MESSAGE);
 		}
 		return 0;            //返回0，表示获取端口号错误，重新输入
+	}
+	
+	//判断此用户是否在服务器端登录，登录返回1，没有登录返回0
+	public int getUserStatus(){
+		String status = "";
+			try {
+				ServerSocket ser = new ServerSocket(clientPort);
+				Socket soc = ser.accept();
+				int len;
+				char[] ch = new char[512];
+				InputStreamReader reader = new InputStreamReader(soc.getInputStream());
+				while ((len = reader.read(ch)) != -1) {
+					status = status + String.valueOf(ch, 0, len);
+				}
+				System.out.println(status);		
+				reader = null;
+				ser.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return Integer.parseInt(status);
 	}
 }
